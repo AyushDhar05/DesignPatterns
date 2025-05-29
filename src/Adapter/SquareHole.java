38d2474694ab0d47372db53b35ee01b66cf38cb8
Ayush Dhar <108949872+AyushDhar05@users.noreply.github.com>
2025-05-29 18:46:46 -0500

    implements Adapter

---
+++ src/Adapter/SquareHole.java
@@ -0,0 +1,13 @@
+package Adapter;
+
+class SquareHole {
+    private double sideLength;
+
+    public SquareHole(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public boolean canFit(Square square) {
+        return this.sideLength >= square.getSideLength();
+    }
+}
